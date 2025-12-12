3cff3984119824dad17d235d49b1668ee900d1e5
OmarZakaria04 <omarzakaria2004@gmail.com>
2025-12-12 16:00:28 +0200

    task4

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,13 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
-import { useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { addItem } from './CartSlice';
 function ProductList({ onHomeClick }) {
     const [showCart, setShowCart] = useState(false);
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
     const [addedToCart, setAddedToCart] = useState({});
     const dispatch = useDispatch();
+    const cartItems = useSelector(state => state.cart.items);
+
+    const calculateTotalQuantity = () => {
+        let total = 0;
+        cartItems.forEach(item => {
+            total += item.quantity;
+        });
+        return total;
+    };
 
     const plantsArray = [
         {
